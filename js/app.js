33ba7d2cf274c81d1e3fde58411e97275b8cc263
Shahrukh Sayyed <35968656+ShahrukhSayyed@users.noreply.github.com>
2018-03-29 21:26:55 +0530

    All functions are modified to ES6

--- js/app.js
+++ js/app.js
@@ -1,10 +1,6 @@
  // main document ready function to check if dom is loaded fully or not
-$( document ).ready(function() {
-    //Classes added for NavBar
-    $('ul li').click(function(){
-        $("li").removeClass("active");
-        $(this).addClass("active");
-    });
+$( document ).ready(() =>{
+
 
     //Hiding the containers on load
     $("#mianContainer").hide();
@@ -12,8 +8,14 @@ $( document ).ready(function() {
     $("#containerCoverPic").hide();
 
 
+    //Classes added for NavBar
+    $('ul li').click(() => {
+        $("li").removeClass("active");
+        $(this).addClass("active");
+    });
 
-    $("#linkBasicDetails").on('click',function(){ //animation to Navbar
+
+    $("#linkBasicDetails").on('click',() => { //animation to Navbar
         $("#basicDetailsCard").show(1000);
         $("#aboutCard").hide(1000);
         $("#educationCard").hide(1000);
@@ -22,9 +24,7 @@ $( document ).ready(function() {
     
     });//end function
 
-    $("#linkAbout").on('click',function(){
-
-
+    $("#linkAbout").on('click',() => {
         $("#basicDetailsCard").hide(1000);
         $("#aboutCard").show(1000);
         $("#educationCard").hide(1000);
@@ -33,7 +33,7 @@ $( document ).ready(function() {
     
     });
 
-    $("#linkEducation").on('click',function(){
+    $("#linkEducation").on('click',() =>{
 
         $("#basicDetailsCard").hide(1000);
         $("#aboutCard").hide(1000);
@@ -44,7 +44,7 @@ $( document ).ready(function() {
     });
 
 
-    $("#linkWork").on('click',function(){
+    $("#linkWork").on('click',() => {
 
         $("#basicDetailsCard").hide(1000);
         $("#aboutCard").hide(1000);
@@ -55,7 +55,7 @@ $( document ).ready(function() {
     });
 
 
-    $("#linkPosts").on('click',function(){
+    $("#linkPosts").on('click',() =>{
 
         $("#basicDetailsCard").hide(1000);
         $("#aboutCard").hide(1000);
@@ -72,14 +72,14 @@ $( document ).ready(function() {
     //Functions for retrieving the data
 	
     	
-	function getFacebookInfo(){
+	let getFacebookInfo =  () => {
 
-        	var myFacebookToken = $("#tokenText").val();
-		localStorage.setItem("Token", myFacebookToken); //storing coockies for Token 
+        	let myFacebookToken = $("#tokenText").val();
+		    localStorage.setItem("Token", myFacebookToken); //storing coockies for Token 
 		
         	$.ajax('https://graph.facebook.com/me?fields=id,name,cover,education,work,birthday,gender,interested_in,languages,quotes,email,address,picture.width(300).height(300),friends&access_token='+myFacebookToken,{
 
-                success : function(response){
+                success : (response) => {
   //                  console.log(response);
   //                  console.log(typeof(response));
                     
@@ -130,7 +130,7 @@ $( document ).ready(function() {
 
 
 
-                    var lang = $.map(response.languages, function(index) {
+                    let lang = $.map(response.languages, (index) => {
                         return index.name;
                     });
 
@@ -161,11 +161,11 @@ $( document ).ready(function() {
                     }
 
 
-                    var friends = $.map(response.friends.data, function(index,value) {
+                    let friends = $.map(response.friends.data, (index,value) =>{
                         return index.name;
                     });
                     
-                    var friendsCounts = response.friends.summary.total_count ;
+                    let friendsCounts = response.friends.summary.total_count ;
 
                     $("#myFriends").text(friends + "+" + friendsCounts + " more" ); 
 
@@ -173,7 +173,7 @@ $( document ).ready(function() {
 
                     //Work 
 
-                    var employer = $.map(response.work, function(index,value) {
+                    let employer = $.map(response.work, (index,value) => {
                         if(index.employer != undefined && index.employer!= null ){
                             return (index.employer.name );
                         }
@@ -182,7 +182,7 @@ $( document ).ready(function() {
                         }
                     });
 
-                    var position = $.map(response.work, function(index,value) {
+                    let position = $.map(response.work, (index,value) =>{
                         if(index.position != undefined && index.position != null){
                             return (index.position.name);
                         }
@@ -191,7 +191,7 @@ $( document ).ready(function() {
                         }
                     });
 
-                    $.each(employer, function(i, item) {
+                    $.each(employer, (i, item) => {
                         if(employer[i] != null && position[i] != null)
                             $("#myEmployer").append("Company : " + "<strong>" + employer[i] + "</strong><br>" + "Position : " + position[i] + "<hr>" );
                     });
@@ -200,7 +200,7 @@ $( document ).ready(function() {
 
                     //Education
 
-                    var colleges = $.map(response.education, function(index,value) {
+                    let colleges = $.map(response.education, (index,value) => {
                         if(index.school != undefined && index.school != null ){
                             return index.school.name;
                         }
@@ -209,7 +209,7 @@ $( document ).ready(function() {
                         }
                     });
 
-                    var concentration = $.map(response.education, function(index,value) {
+                    let concentration = $.map(response.education, (index,value) => {
                         if(index.concentration != undefined && index.concentration != null){
                             return (index.concentration[0].name);
                             //console.log(index.concentration[0].name);
@@ -219,7 +219,7 @@ $( document ).ready(function() {
                         }
                     });
 
-                    $.each(colleges, function(i, item) {
+                    $.each(colleges, (i, item) => {
                         if(concentration[i] != null && colleges[i] != null)
                             //console.log(colleges[i]);
                             $("#myEducation").append("School Name : " + "<strong>" + colleges[i] + "</strong><br>" + "Concentration : "+ concentration[i] + "<hr>" );
@@ -238,17 +238,17 @@ $( document ).ready(function() {
                 },
 
                 timeout: 1500, // keeping the timeout for 1.5 sec 
-                beforeSend: function () { //Displaying loader
+                beforeSend: () => { //Displaying loader
                     $('#loader').delay(1500).show();
                     $("#mianContainer").hide();
 
                 },
-                complete: function () {
+                complete: () => {
                     $('#loader').delay(1500).hide(); // hide the loader on screen 
 
                 },
 
-                error: function (req, status, error) { // error function for showing the error on console and giving warining to users via alert
+                error: (req, status, error) => { // error function for showing the error on console and giving warining to users via alert
                     $('#loader').delay(1500).hide(); // hide the loader on screen 
                     $("#mianContainer").hide();
                     $("#containerCoverPic").hide();
